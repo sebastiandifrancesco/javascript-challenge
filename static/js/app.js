a88388277d67d093762c3675b5fc13ecbbf4cb41
Sebastian Di Francesco <sebastiandifrancesco@gmail.com>
2021-03-29 08:38:18 -0400

    finalized

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// Reference
+var tbody = d3.select("tbody");
+
+// Add data
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input
+    var inputElement = d3.select("#input");
+    // Get the value property
+    var inputValue = inputElement.property("value");
+    // Filter Data
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
+                                                    sighting.city === inputValue ||
+                                                    sighting.state === inputValue ||
+                                                    sighting.country === inputValue ||
+                                                    sighting.shape === inputValue);
+
+    filteredData.forEach(function(selections) {
+
+    // Append one table row `tr` for each sighting
+    var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
